e2fcc7d8dd55e4665bc32a71acd5e91f9edba1c1
João Mangueira <jamjs7@gmail.com>
2023-01-16 10:03:33 -0300

    aplicação finalizando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+    const [input, setInput] = useState('');
+    const [cep, setCep] = useState({});
+
+    async function handleSearch() {
+        if (input === '') {
+            alert('Preencha algum CEP!');
+            return;
+        }
+
+        try {
+            const response = await api.get(`${input}/json`);
+            setCep(response.data)
+        } catch (error) {
+            alert('Erro ao buscar endereço.');
+        } finally {
+            setInput('');
+        }
+    }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
         <div className="containerInput">
-            <input type="text" placeholder="Digite o seu cep..." />
-            <button className="buttonSearch">
+            <input
+                type="text"
+                placeholder="Digite o seu cep..."
+                value={input}
+                onChange={(event) => setInput(event.target.value)}
+            />
+            <button className="buttonSearch" onClick={handleSearch}>
                 <FiSearch size={25} color="#FFF" />
             </button>
         </div>
-        <main className="main">
-            <h2>CEP: 79003222</h2>
-            <span>Rua teste algum</span>
-            <span>complemento: Algum complemento</span>
-            <span>Vila rosa</span>
-            <span>São Paulo - SP</span>
-        </main>
+        {Object.keys(cep).length > 0 && (
+            <main className="main">
+                <h2>CEP: {cep.cep}</h2>
+                <span>{cep.logradouro}</span>
+                <span>complemento: {cep.complemento}</span>
+                <span>{cep.bairro}</span>
+                <span>{cep.localidade} - {cep.uf}</span>
+            </main>
+        )}
     </div>
   );
 }
